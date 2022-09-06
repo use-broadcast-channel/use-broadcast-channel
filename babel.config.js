8fb5fb48b6b86e152cda74bcc48dd43881ce0eb4
Jusung Hwang (nabigraphics) <hbh2692@naver.com>
2022-09-07 02:44:19 +0900

    chore: fix prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(true);
   return {
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  }
-}
+  };
+};
